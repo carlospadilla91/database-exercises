689070bc7c16d21eef4d272c1a14521a7ee99bb1
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-03-29 16:38:28 -0500

    feat: adding first sql file for tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,24 @@
+-- USE codeup_test_db;
+--
+-- DROP TABLE IF EXISTS albums;
+--
+-- CREATE TABLE albums (
+--     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     artist VARCHAR(75) NOT NULL,
+--     name VALUE(75) NOT NULL,
+--     release_date INT UNSIGNED NOT NULL,
+--     sales DECIMAL(10,2) UNSIGNED NOT NULL,
+--     genre VARCHAR(50) NOT NULL,
+--     PRIMARY KEY(id)
+
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR (100) NOT NULL,
+    name VARCHAR (100) NOT NULL,
+    release_date INT NOT NULL,
+    sales DECIMAL(6, 3) NOT NULL,
+    genre VARCHAR (100),
+    PRIMARY KEY (id)
+);
