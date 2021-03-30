e2eb7878ca5bd6183867e41f90acc162ff0163ed
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-03-30 12:32:20 -0500

    feat: completed first part of basic statements exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -16,9 +16,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR (100) NOT NULL,
-    name VARCHAR (100) NOT NULL,
+    album_name VARCHAR (100) NOT NULL,
     release_date INT NOT NULL,
-    sales DECIMAL(6, 3) NOT NULL,
+    sales DECIMAL(10, 2) NOT NULL,
     genre VARCHAR (100),
     PRIMARY KEY (id)
 );
