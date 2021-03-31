135e018b56853e5d90d3f2e18f9d57418b23ec07
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-03-30 20:37:39 -0500

    refactor some code

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,16 +1,3 @@
--- USE codeup_test_db;
---
--- DROP TABLE IF EXISTS albums;
---
--- CREATE TABLE albums (
---     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
---     artist VARCHAR(75) NOT NULL,
---     name VALUE(75) NOT NULL,
---     release_date INT UNSIGNED NOT NULL,
---     sales DECIMAL(10,2) UNSIGNED NOT NULL,
---     genre VARCHAR(50) NOT NULL,
---     PRIMARY KEY(id)
-
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
